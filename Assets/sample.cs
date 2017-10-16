0566a949f922966d32e22fcc4024d44a4154e670
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-10-16 17:30:44 +0900

    first commit

---
+++ Assets/sample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //追加
+
+public class sample : MonoBehaviour
+{
+
+    public Text text; //追加
+
+    // Use this for initialization
+    void Start()
+    {
+        text.text = "Hello Github!"; //追加
+    }
+
+    // Update is called once per fram
+    void Update()
+    {
+
+    }
+}
